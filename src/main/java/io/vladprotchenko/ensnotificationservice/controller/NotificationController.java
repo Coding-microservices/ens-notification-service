5e43ec91b9a9cb39b2de7208000a0dc1425054ab
Vladislav Protchenko <vlad.freezzy@gmail.com>
2025-05-13 16:24:23 +0300

    add swagger and security configs, change controllers

--- src/main/java/io/vladprotchenko/ensnotificationservice/controller/NotificationController.java
+++ src/main/java/io/vladprotchenko/ensnotificationservice/controller/NotificationController.java
@@ -1,9 +1,13 @@
 package io.vladprotchenko.ensnotificationservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import io.vladprotchenko.ensnotificationservice.model.dto.kafka.NotificationKafka;
 import io.vladprotchenko.ensnotificationservice.model.dto.response.NotificationResponse;
 import io.vladprotchenko.ensnotificationservice.service.NotificationService;
+import io.vladprotchenko.ensstartercore.exception.EnsServiceException;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,47 +16,54 @@ import java.util.List;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
+@RequestMapping("/api/v1/notifications")
 @RequiredArgsConstructor
+@Tag(name = "Notifications API")
 public class NotificationController {
 
     private final NotificationService notificationService;
 
-    @PostMapping("/{id}")
+    @Operation(summary = "Create notification", description = "Create notification by template")
+    @PostMapping("/{templateId}")
     public ResponseEntity<String> notify(
             @RequestHeader Long userId,
-            @PathVariable("id") Long templateId
+            @PathVariable("templateId") Long templateId
     ) {
         return ResponseEntity.status(OK).body(notificationService.distributeNotifications(userId, templateId));
     }
 
-    @PostMapping("/{id}/sent")
+    @Operation(summary = "Set as sent", description = "Set notification status as sent")
+    @PostMapping("/{notificationId}/sent")
     public ResponseEntity<NotificationResponse> setNotificationAsSent(
             @RequestHeader Long userId,
-            @PathVariable("id") Long notificationId
+            @PathVariable("notificationId") Long notificationId
     ) {
         return ResponseEntity.status(OK).body(notificationService.setNotificationAsSent(userId, notificationId));
     }
 
-    @PostMapping("/{id}/error")
+    @Operation(summary = "Set as error", description = "Set notification status as error")
+    @PostMapping("/{notificationId}/error")
     public ResponseEntity<NotificationResponse> setNotificationAsError(
             @RequestHeader Long userId,
-            @PathVariable("id") Long notificationId
+            @PathVariable("notificationId") Long notificationId
     ) {
         return ResponseEntity.status(OK).body(notificationService.setNotificationAsError(userId, notificationId));
     }
 
-    @PostMapping("/{id}/corrupt")
+    @Operation(summary = "Set as corrupt", description = "Set notification status as corrupt")
+    @PostMapping("/{notificationId}/corrupt")
     public ResponseEntity<NotificationResponse> setNotificationAsCorrupt(
             @RequestHeader Long userId,
-            @PathVariable("id") Long notificationId
+            @PathVariable("notificationId") Long notificationId
     ) {
         return ResponseEntity.status(OK).body(notificationService.setNotificationAsCorrupt(userId, notificationId));
     }
 
-    @PostMapping("/{id}/resending")
+    @Operation(summary = "Set as resending", description = "Set notification status as resending")
+    @PostMapping("/{notificationId}/resending")
     public ResponseEntity<NotificationResponse> setNotificationAsResending(
             @RequestHeader Long userId,
-            @PathVariable("id") Long notificationId
+            @PathVariable("notificationId") Long notificationId
     ) {
         return ResponseEntity.status(OK).body(notificationService.setNotificationAsResending(userId, notificationId));
     }
@@ -67,5 +78,4 @@ public class NotificationController {
                 notificationService.getNotificationsForRebalancing(pendingSec, newSec, size)
         );
     }
-
 }
